9d4237302628246a2540975353f433f1803ca6d3
tayyabhussain2910 <50795716+TayyabHussain03@users.noreply.github.com>
2021-05-31 19:01:56 +0500

    question2 added

--- app.js
+++ app.js
@@ -5,3 +5,13 @@
 //     console.log(today)
 // }
 // date()
+
+
+// **********QUESTION 2**********
+
+// function xyz(){
+//     firstName = prompt("Enter Your First Name")
+//     lastName = prompt("Enter Your Last Name")
+//     alert("Welcome! " + firstName + " " + lastName)
+// }
+// xyz()
