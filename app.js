ac3baab1cb89bffff6edbdf07e8269b084c3e85f
tayyabhussain2910 <50795716+TayyabHussain03@users.noreply.github.com>
2021-05-31 19:09:27 +0500

    question3 added

--- app.js
+++ app.js
@@ -15,3 +15,14 @@
 //     alert("Welcome! " + firstName + " " + lastName)
 // }
 // xyz()
+
+
+// **********QUESTION 3**********
+
+// function add(){
+//     var num1 = +prompt("Enter First Number Here")
+//     var num2 = +prompt("Enter Second Number Here")
+//     addition = num1 + num2
+//     alert(addition)
+// }
+// add()
